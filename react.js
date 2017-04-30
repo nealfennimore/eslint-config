b09c520c99bd47eaa7731f494bfe82e2c01888d0
Neal Fennimore <hi@neal.codes>
2017-04-30 00:09:26 -0700

    Update config. Separate react specific items

---
+++ react.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": [
+        "plugin:react/recommended",
+        "nfen/lib/defaults"
+    ],
+    "plugins:" [
+        "react"
+    ],
+    "rules": {
+        "react/jsx-uses-vars": 1,
+        "react/react-in-jsx-scope": 1,
+        "react/prop-types": 1
+    }
+}
