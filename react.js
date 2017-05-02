de9006561b479c583927293dbf0776ea5f71fd78
Neal Fennimore <hi@neal.codes>
2017-05-01 20:52:56 -0700

    Update to allow jsx in other files

--- react.js
+++ react.js
@@ -4,10 +4,11 @@ module.exports = {
         "nfen/lib/defaults"
     ],
     "rules": {
-        "react/jsx-indent": [2, 4],
+        "react/jsx-filename-extension": 0,
         "react/jsx-indent-props": [2, 4],
+        "react/jsx-indent": [2, 4],
         "react/jsx-uses-vars": 1,
-        "react/react-in-jsx-scope": 1,
-        "react/prop-types": 1
+        "react/prop-types": 1,
+        "react/react-in-jsx-scope": 1
     }
 }
